249fe1de5eca2e5b2a2d27ec372a6f26c847753e
Annelies11 <mmahmudi1103@gmail.com>
2024-09-21 21:45:25 +0700

    update context

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,13 @@
+import { useQuiz } from "../contexts/QuizContext";
 import Option from "./Option";
-function Question({ question, dispatch, answer }) {
+function Question() {
   // console.log(question);
+  const { questions, index } = useQuiz();
+  const question = questions.at(index);
   return (
     <div>
       <h4>{question.question}</h4>
-      <Option question={question} dispatch={dispatch} answer={answer} />
+      <Option question={question} />
     </div>
   );
 }
